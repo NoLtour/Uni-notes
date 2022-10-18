47d7a05748c5d2dbd0ce3f191492bb252160b484
Syndicalism <71135237+Syndicalism@users.noreply.github.com>
2022-10-18 21:03:29 +0100

    vault backup: 2022-10-18 21:03:29

--- Programming stuff/python idk/Aero/1 Manths and Numerical concepts/TestT1Q10.py
+++ Programming stuff/python idk/Aero/1 Manths and Numerical concepts/TestT1Q10.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy.interpolate as spInt
 import matplotlib.pyplot as plot
 
 dx = 0.1
@@ -11,7 +12,7 @@ yPoints = [6.23, 5.97, 6.07, 6.97, 8.04, 7.72, 7.54, 6.79, 7.9, 8.43]
 plot.figure( 69 );
 
 plot.plot( xPoints, yPoints, "kx" );
-plot.plot( np.arange(160,340,1), np.interp( np.arange(160,340,1), xPoints, yPoints )  );
+plot.plot( np.arange(160,340,1), spInt.interp1d(  xPoints, yPoints )  );
 
 print( np.interp( 303, xPoints, yPoints ) )
 
