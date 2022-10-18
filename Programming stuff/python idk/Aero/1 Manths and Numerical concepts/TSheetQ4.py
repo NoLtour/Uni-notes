21a1cbbc6dc7bdff6c4e30a82fa23e6bdc666edb
Syndicalism <71135237+Syndicalism@users.noreply.github.com>
2022-10-18 11:30:16 +0100

    awd

--- Programming stuff/python idk/Aero/1 Manths and Numerical concepts/TSheetQ4.py
+++ Programming stuff/python idk/Aero/1 Manths and Numerical concepts/TSheetQ4.py
@@ -3,7 +3,26 @@ import matplotlib.pyplot as plot
 
 dx = 0.1
 
-xPoints = np.arange( -5, 5, dx )
+dr = 0.1
+
+rMax = 15
+
+R = 2
+
+# r = 0 -> rMax
+rP1 = np.arange( 0, R, dr )
+rP2 = np.arange( R, rMax, dr )
+
+T = 5
+
+# u_theta = func(r)
+uThP1 = T*rP1/(R**2)
+uThP2 = T/rP2
+uThP = uThP1 + uThP2
+
+# u_r = 0
+uRP = np.repeat( 0, uThP )
+
 
 test = xPoints ** 0.5
 
