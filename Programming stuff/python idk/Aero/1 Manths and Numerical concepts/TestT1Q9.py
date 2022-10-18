289974d7d3824d583ccc3670bce9e5f90ee5cbbc
Syndicalism <71135237+Syndicalism@users.noreply.github.com>
2022-10-18 20:40:29 +0100

    vault backup: 2022-10-18 20:40:29

--- Programming stuff/python idk/Aero/1 Manths and Numerical concepts/TestT1Q9.py
+++ Programming stuff/python idk/Aero/1 Manths and Numerical concepts/TestT1Q9.py
@@ -1,8 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plot
 
-dx = 0.25
-dy = 0.25
+dx = 0.1
+dy = 0.1
 
 axisSize = 4
 
@@ -15,13 +15,13 @@ X, Y = np.meshgrid( xPoints, yPoints );
 xVels = 5*X*(Y**2)
 yVels = X*(Y**3)
 
-xDelMod = np.add( np.gradient( xVels ) , np.gradient( yVels ) )
+xDelMod = np.abs( np.add( np.gradient( xVels )[0] , np.gradient( yVels )[1] ) )
 
 plot.figure( 69 );
 
-plot.quiver( X, Y, xVels, yVels )
+#plot.quiver( X, Y, xVels, yVels )
 
-#plot.colorbar( plot.contourf( X, Y, np.gradient( xVels ), 8 ) )
+plot.colorbar( plot.contourf( X, Y, xDelMod ) )
 #plot.plot( xPoints, intPoints, "bx" );
 
 plot.show();
