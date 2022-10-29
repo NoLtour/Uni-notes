a96c78c6f2d6b0839b7ce344fc5fef21a9c0bf6e
Syndicalism <71135237+Syndicalism@users.noreply.github.com>
2022-10-29 15:03:26 +0100

    vault backup: 2022-10-29 15:03:26

--- Programming stuff/python idk/Aero/2/1 First bit/gettingThick.py
+++ Programming stuff/python idk/Aero/2/1 First bit/gettingThick.py
@@ -3,10 +3,10 @@ import pandas as pd
 import numpy as np
 import os
 
-directory_path = os.getcwd()
+directory_path = "C:\\uni stuff\\Uni notes\\Programming stuff\\python idk\Aero\\2\\1 First bit"
 
 laminarSet = pd.read_csv( directory_path+"\\laminar_profile.csv" )
-turbulantSet = pd.read_csv( "//turbulent_profile.csv" )
+turbulantSet = pd.read_csv( directory_path+"\\turbulent_profile.csv" )
 
 
 def getBLThickness( distances, velocities ):
@@ -15,10 +15,10 @@ def getBLThickness( distances, velocities ):
     return np.interp( distances, velocities, FS_Vel )
 
 
-print( "laminar thickness:", getBLThickness( laminarSet.columns[0], laminarSet.columns[1] )  )
+print( "laminar thickness:", getBLThickness( laminarSet.columns[0][1:], laminarSet.columns[1][1:] )  )
 
 
-print( "turbulant thickness:", getBLThickness( turbulantSet.columns[0], turbulantSet.columns[1] )  )
+print( "turbulant thickness:", getBLThickness( turbulantSet.columns[0][1:], turbulantSet.columns[1][1:] )  )
 
 
 
