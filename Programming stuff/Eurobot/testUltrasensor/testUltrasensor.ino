c59d1cde6f6ada98eea0672fcb8ed32103357fc8
Syndicalism <71135237+Syndicalism@users.noreply.github.com>
2023-02-07 23:13:19 +0000

    vault backup: 2023-02-07 23:13:19

--- Programming stuff/Eurobot/testUltrasensor/testUltrasensor.ino
+++ Programming stuff/Eurobot/testUltrasensor/testUltrasensor.ino
@@ -5,8 +5,8 @@
 // Using HC-SR04 Module
 // Tested on 17 September 2019
 // ---------------------------------------------------------------- //
-#define echoPin 2 // attach pin D2 Arduino to pin Echo of HC-SR04
-#define trigPin 12 //attach pin D3 Arduino to pin Trig of HC-SR04
+#define echoPin 12 // attach pin D2 Arduino to pin Echo of HC-SR04
+#define trigPin 2 //attach pin D3 Arduino to pin Trig of HC-SR04
 // defines variables
 long duration; // variable for the duration of sound wave travel
 int distance; // variable for the distance measurement
